aa44c7d73ae105cfc9e793a435daf0d4cd5ea245
Weihao Ming <mingweihao@gmail.com>
2014-08-13 17:35:54 +0100

    first init

---
+++ core/src/cn/mingweihao/MolecularDynamics/MainScreen.java
@@ -0,0 +1,79 @@
+package cn.mingweihao.MolecularDynamics;
+
+
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Circle;
+
+public class MainScreen implements Screen {
+    MolecularDynamics game;
+
+	Map map;
+
+    Texture ballImage;
+    OrthographicCamera camera;
+
+
+
+    public MainScreen(MolecularDynamics game) {
+        this.game = game;
+
+        ballImage = new Texture(Gdx.files.internal("XeMolecular.png"));
+
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
+    }
+	
+
+    @Override
+    public void show() {
+        map = new Map();
+    }
+
+	@Override
+	public void render (float delta) {
+        delta = Math.min(0.06f, Gdx.graphics.getDeltaTime());
+        map.update(delta);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+        game.batch.setProjectionMatrix(camera.combined);
+        game.batch.begin();
+        for(int i = 0; i < map.balls.size; i++) {
+            game.batch.draw(ballImage, map.balls.get(i).pos.x, map.balls.get(i).pos.y, 20f, 20f);
+        }
+        game.batch.end();
+
+	}
+
+    public void dispose() {
+
+    }
+
+
+    public void resume() {
+
+    }
+
+    public void pause() {
+
+    }
+
+    public void resize(int width, int height) {
+
+    }
+
+
+    public void hide() {
+
+    }
+
+
+}
